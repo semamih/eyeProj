50b367fa1f920b3c7fd4efb65c2f615f39031158
Sema <semamihailov05@gmail.com>
2024-09-17 11:29:45 +0300

    added scrolls and footer

--- js/script.js
+++ js/script.js
@@ -26,3 +26,54 @@ function prevSlide() {
 }
 
 showSlide(currentIndex);
+
+
+// Функция для плавного скролла
+function scrollToSectionCenter(event, sectionId) {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+}
+function scrollToSection(event, sectionId) {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+
+    // Прокрутить секцию в видимую область
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        top: -30,
+    });
+
+    const offset = 30;
+    const scrollPosition = section.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({
+        top: scrollPosition,
+        behavior: 'smooth'
+    });
+}
+
+document.querySelector('.preAboutLink').addEventListener('click', (event) => scrollToSectionCenter(event, 'preAbout'));
+document.querySelector('.preAboutLink-2').addEventListener('click', (event) => scrollToSectionCenter(event, 'preAbout'));
+
+document.querySelector('.header-button').addEventListener('click', (event) => scrollToSectionCenter(event, 'buy'));
+
+document.querySelector('.conditionsLink').addEventListener('click', (event) => scrollToSection(event, 'conditions'));
+document.querySelector('.conditionsLink-2').addEventListener('click', (event) => scrollToSection(event, 'conditions'));
+
+document.querySelector('.resultLink').addEventListener('click', (event) => scrollToSection(event, 'result'));
+document.querySelector('.resultLink-2').addEventListener('click', (event) => scrollToSection(event, 'result'));
+
+document.querySelector('.feedbackLink').addEventListener('click', (event) => scrollToSection(event, 'feedback'));
+document.querySelector('.feedbackLink-2').addEventListener('click', (event) => scrollToSection(event, 'feedback'));
+
+document.querySelector('.questionsLink').addEventListener('click', (event) => scrollToSection(event, 'questions'));
+document.querySelector('.questionsLink-2').addEventListener('click', (event) => scrollToSection(event, 'questions'));
+
+
+
+
